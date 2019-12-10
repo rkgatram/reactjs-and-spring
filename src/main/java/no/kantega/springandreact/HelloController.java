be1e7d053e541c673fb651a72741277c5afec35c
Ravi Kumar Gatram <ravikumar.g@hcl.com>
2019-12-10 10:14:59 +0530

    first commit

---
+++ src/main/java/no/kantega/springandreact/HelloController.java
@@ -0,0 +1,19 @@
+package no.kantega.springandreact;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello, the time at the server is now " + new Date() + "\n";
+    }
+    
+    @GetMapping("/api/test")
+    public String greeting() {
+        return "Current time at the server is : " + new Date() + "\n";
+    }
+}
